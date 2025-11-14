88ab55dc7ef5dfe51f18266f7b4d8c07e132d22c
hbrzq <239223639+hbrzq@users.noreply.github.com>
2025-11-14 17:24:46 -0500

    minimized

--- src/app/screens/main/MainScreen.ts
+++ src/app/screens/main/MainScreen.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { FancyButton } from "@pixi/ui";
 import { animate } from "motion";
 import type { AnimationPlaybackControls } from "motion/react";
